52a2684dcf63cfc631ded808ff138c6656a79ddd
VeeraDurgarao <veeradurga.rao@brainvire.com>
2024-07-25 11:13:52 +0530

    popup task

--- bank/models/temporary.py
+++ bank/models/temporary.py
@@ -21,8 +21,21 @@ class ProductCategoryController(http.Controller):
     @http.route('/product/<model("product.template"):product>', type='http',
                 auth="public", website=True)
     def product_details(self, product, **kwargs):
-        # result = request.env['product.template'].search([])
-        # print(result.qty_available)
+        stock_quant = request.env['stock.quant'].search([('product_id', '=', product.product_variant_id.id)],
+                                                        order='id asc', limit=1)
+        value = 0
+        for i in stock_quant:
+            value += i.inventory_quantity_auto_apply
+        new_item = stock_quant.inventory_quantity_auto_apply if stock_quant else 0
+        stock_change_qty = request.env['stock.change.product.qty'].search(
+            [('product_id', '=', product.product_variant_id.id)], order='id asc', limit=1)
+        new_item2 = stock_change_qty.new_quantity if stock_change_qty else 0
+
+        print(f"Inventory Quantity: {new_item}")
+        print(f"New Quantity: {new_item2}")
+
+        result = request.env['product.template'].search([])
+        print(result)
         if product.qty_available > 0:
             return request.render('website_tasks.product_details', {
                 'product': product,
